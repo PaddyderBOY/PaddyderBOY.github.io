8d06dd62f10295ce414666409378c33a31e85cc3
Patrick Pap <patrick-pap@web.de>
2018-01-19 11:38:08 +0100

    v9

--- static/main.js
+++ static/main.js
@@ -46,8 +46,90 @@ function main() {
                                                             autoplaySpeed: 8000,
                                                             dots: true,
                                                             arrows: true,
-                                                        })
+                                                        });
+                                                        var bar = new ProgressBar.Circle('#progressbarAlternativeHTC',{
+                                                            color: '#aaa',
+                                                            // This has to be the same size as the maximum width to
+                                                            // prevent clipping
+                                                            strokeWidth: 4,
+                                                            trailWidth: 1,
+                                                            easing: 'easeInOut',
+                                                            duration: 1400,
+                                                            from: { color: '#aaa', width: 1 },
+                                                            to: { color: '#5f0f4e', width: 4 },
+                                                            // Set default step function for all animate calls
+                                                            step: function(state, circle) {
+                                                                circle.path.setAttribute('stroke', state.color);
+                                                                circle.path.setAttribute('stroke-width', state.width);
+
+                                                                var value = Math.round(circle.value() * 100);
+                                                                if (value === 0) {
+                                                                    circle.setText('');
+                                                                } else {
+                                                                    circle.setText(value+'%');
+                                                                }
+
+                                                            }
+                                                        });
+                                                        bar.text.style.fontFamily = '"Lato", sans-serif';
+                                                        bar.text.style.fontSize = '2rem';
+                                                        bar.animate(0.86);
+                                                        var bar2 = new ProgressBar.Circle('#progressbarAlternativeIphone',{
+                                                            color: '#aaa',
+                                                            // This has to be the same size as the maximum width to
+                                                            // prevent clipping
+                                                            strokeWidth: 4,
+                                                            trailWidth: 1,
+                                                            easing: 'easeInOut',
+                                                            duration: 1400,
+                                                            from: { color: '#aaa', width: 1 },
+                                                            to: { color: '#5f0f4e', width: 4 },
+                                                            // Set default step function for all animate calls
+                                                            step: function(state, circle) {
+                                                                circle.path.setAttribute('stroke', state.color);
+                                                                circle.path.setAttribute('stroke-width', state.width);
+
+                                                                var value = Math.round(circle.value() * 100);
+                                                                if (value === 0) {
+                                                                    circle.setText('');
+                                                                } else {
+                                                                    circle.setText(value+'%');
+                                                                }
+
+                                                            }
+                                                        });
+                                                        bar2.text.style.fontFamily = '"Lato", sans-serif';
+                                                        bar2.text.style.fontSize = '2rem';
+                                                        bar2.animate(0.70);
+                                                        var bar3 = new ProgressBar.Circle('#progressbarAlternativeSTablet',{
+                                                            color: '#aaa',
+                                                            // This has to be the same size as the maximum width to
+                                                            // prevent clipping
+                                                            strokeWidth: 4,
+                                                            trailWidth: 1,
+                                                            easing: 'easeInOut',
+                                                            duration: 1400,
+                                                            from: { color: '#aaa', width: 1 },
+                                                            to: { color: '#5f0f4e', width: 4 },
+                                                            // Set default step function for all animate calls
+                                                            step: function(state, circle) {
+                                                                circle.path.setAttribute('stroke', state.color);
+                                                                circle.path.setAttribute('stroke-width', state.width);
+
+                                                                var value = Math.round(circle.value() * 100);
+                                                                if (value === 0) {
+                                                                    circle.setText('');
+                                                                } else {
+                                                                    circle.setText(value+'%');
+                                                                }
+
+                                                            }
+                                                        });
+                                                        bar3.text.style.fontFamily = '"Lato", sans-serif';
+                                                        bar3.text.style.fontSize = '2rem';
+                                                        bar3.animate(0.56);
                                                     });
+                                                    var line = new ProgressBar.Line('#progressbarAlternative');
                                                 });
                                             });
                                         });
@@ -99,3 +181,4 @@ $(window).bind('scroll', function() {
         $('.navbar-default').removeClass('on');
     }
 });
+
